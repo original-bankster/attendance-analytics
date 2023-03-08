c68d55367dee2c60c4513ce1f95dfd930184a9e5
original-bankster <123207765+original-bankster@users.noreply.github.com>
2023-03-08 08:33:32 -0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+" > ~/.streamlit/config.toml
